c63d7b95549e4de90f89308beffbe38210213de6
cmoon <calmoon808@gmail.com>
2022-03-20 15:57:57 -1000

    Cleaned up loginPage

--- react-frontend/src/components/LoginPage/LoginPage.jsx
+++ react-frontend/src/components/LoginPage/LoginPage.jsx
@@ -1,26 +1,8 @@
 import React, { useState } from 'react'
 import './LoginPage.scss';
-// import { ethers } from 'ethers';
-// import anemonethJSON from '../../utils/anemoneth.json'
-// import { AuthContext } from "../../App";
 import LoginRegisterComponent from '../LoginRegisterComponent/LoginRegister'
 
 function LoginPage() {
-  // const [userAccount, setUserAccount] = useState("");
-  // const [userBalance, setUserBalance] = useState("");
-  
-  // DELETE THIS FUNCTION
-  const handleClick = async () => {
-    try {
-      const response = await fetch('http://localhost:8080/', {mode:'cors'});
-      const data = await response.json();
-      console.log({ data })
-    }
-    catch (e) {
-      console.log(e)
-    }
-  }
-
   return (
     <div className='loginpage'>
       <div className='loginPiecesContainer'>
@@ -28,8 +10,6 @@ function LoginPage() {
           <h1>Anemoneth</h1>
           <h4>Connect with friends and the decentralised world around you on Anemoneth</h4>
         </div>
-        {/**DELETE THIS BUTTON */}
-        <button onClick={handleClick}>test</button>
         <div className='loginPieces'>
           <LoginRegisterComponent/>
         </div>
