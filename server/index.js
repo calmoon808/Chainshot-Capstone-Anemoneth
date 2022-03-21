1bb2bf97272c34e8ae291a2b2420ce1b254f5282
cmoon <calmoon808@gmail.com>
2022-03-20 16:00:44 -1000

    /fileUpload route renamed to /postData. route logic also modified to handle if file exists or not.

--- server/index.js
+++ server/index.js
@@ -1,12 +1,10 @@
 const express = require("express");
-// const request = require('request');
-
 const fileUpload = require('express-fileupload');
 const fs = require("fs");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 8080;
-const { isUser, mkDir, writeFile } = require("./helper");
+const { isUser, mkUserDir, writeFile, getUserPosts } = require("./helper");
 const { create } = require('ipfs-http-client');
 const ipfs = create('/ip4/127.0.0.1/tcp/5001');
 
@@ -16,8 +14,48 @@ app.use(express.urlencoded({ extended: true })); // body-parser
 app.use(express.json()); // for JSON payloads
 
 app.get("/", (req, res) => {
-    
     res.status(200).send("test");
+})
+
+app.get("/userPosts", async (req, res) => {
+    const userWalletAddress = req.query.userWalletAddress;
+    res.status(200).send(await getUserPosts(userWalletAddress));
+})
+
+app.post("/postUpload", async (req, res) => {
+    const userAddress = req.body.userAddress;
+    if (!(await isUser(`/${userAddress}`))) {
+        await mkUserDir(userAddress);
+    }
+    const postTitle = req.body.postTitle;
+    const postBody = req.body.postBody;
+
+    const postData = {
+        userAddress,
+        postTitle,
+        postBody,
+    }
+    let postObj = { owner: userAddress };
+    if (req.files) {
+        const file = req.files.postFile;
+        postData.fileName = file.name;
+        postData.filePath = "tempFiles/" + file.name;
+        file.mv(postData.filePath, async (err) => {
+            if (err) {
+                console.log("Error: failed to download the file");
+                return res.status(500).send(err);
+            }
+    
+            postObj = await writeFile(postData);
+            fs.unlink(postData.filePath, (err) => {
+                if (err) console.log(err);
+            })
+            res.status(200).send(postObj);
+        })
+    } else {
+        postObj = await writeFile(postData);
+        res.status(200).send(postObj);
+    }
 })
 
 app.post("/stringUpload", async(req, res) => {
@@ -29,30 +67,7 @@ app.post("/stringUpload", async(req, res) => {
     await ipfs.files.write(`/${userAddress}/comments/${req.body.string}`, req.body.string, { create: true })
     let result = await ipfs.files.stat(`/${userAddress}/comments/${req.body.string}`);
     console.log(result);
-})
-
-app.post("/fileUpload", async (req, res) => {
-    const userAddress = req.body.userAddress;
-    if (!(await isUser(`/${userAddress}`))) {
-        await mkDir(userAddress);
-    }
-    const file = req.files.file;
-    const fileName = req.files.file.name;
-    const filePath = "tempFiles/" + fileName;
-
-    file.mv(filePath, async (err) => {
-        if (err) {
-            console.log("Error: failed to download the file");
-            return res.status(500).send(err);
-        }
-
-        const fileHash = await writeFile(userAddress, filePath, fileName);
-        fs.unlink(filePath, (err) => {
-            if (err) console.log(err);
-        })
-        
-        res.status(200).send({ fileHash, fileName });
-    })
+    res.status(200).send()
 })
 
 app.listen(port, () => {
