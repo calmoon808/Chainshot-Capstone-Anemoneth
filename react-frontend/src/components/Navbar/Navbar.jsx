e22de747337cdf7a3f4b8415e5f1675878406195
cmoon <calmoon808@gmail.com>
2022-03-21 14:50:32 -1000

    Moved username modal to navbar as temp location

--- react-frontend/src/components/Navbar/Navbar.jsx
+++ react-frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,22 @@
 import React, { useState, useEffect } from 'react'
+import {Button} from 'react-bootstrap';
+import UserNameInfo from "../UserNameInfo/UserNameInfo";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './Navbar.scss'
 
 function Navbar() {
+  const [modalShow, setModalShow] = useState(false);
 
   return (
     <div>
         <h1>THIS BE THE NAVBAR</h1>
+        <Button variant="primary" onClick={() => setModalShow(true)}>
+          Change username
+        </Button>
+        <UserNameInfo
+          show={modalShow}
+          onHide={() => setModalShow(false)}
+      />
     </div>
   )
 }
