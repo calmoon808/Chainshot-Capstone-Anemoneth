8313eab46ac8183edf86ec3eae806147cd2f15b1
cmoon <calmoon808@gmail.com>
2022-03-21 18:13:57 -1000

    changed AddPostComponent into a Modal and added a timer to delay the homepage loading too fast and causing an error

--- react-frontend/src/components/AddPostComponent/AddPostComponent.jsx
+++ react-frontend/src/components/AddPostComponent/AddPostComponent.jsx
@@ -1,46 +1,86 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
+import {Modal, Button, Form} from 'react-bootstrap'; 
 import './AddPostComponent.scss';
 import axios from 'axios';
 
 function AddPostComponent(props) {
-    const handleFileSubmit = async (e) => {
-        if (!(e.target.elements.title.value)) {
-            alert("Post needs a title!");
-            return;
-        }
-        if (!(e.target.elements.body.value)) {
-            alert("Post needs a body!");
-            return;
-        }
-        const formData = new FormData();
-        formData.append("userAddress", props.userWalletAddress);
-        formData.append("postTitle", e.target.elements.title.value);
-        formData.append("postBody", e.target.elements.body.value);
-        if (e.target.elements[2].files.length) {
-            const file = e.target.elements[2].files[0];
-            formData.append("postFile", file);
-        }
-        axios.post("/postUpload", formData)
-        .then((res) => {
-            console.log(res.data);
-        })
-        .catch(err => {
-            console.log(err);
-        })
+  const handlePostSubmit = async (e) => {
+    e.preventDefault();
+    const postTitle = e.target.elements[0].value;
+    const postBody = e.target.elements[1].value;
+    if (!(postTitle)) {
+        alert("Post needs a title!");
+        return;
     }
+    if (!(postBody)) {
+        alert("Post needs a body!");
+        return;
+      }
+    const formData = new FormData();
+    formData.append("userAddress", props.userWalletAddress);
+    formData.append("postTitle", postTitle);
+    formData.append("postBody", postBody);
+    if (e.target.elements[2].files.length) {
+      const file = e.target.elements[2].files[0];
+      formData.append("postFile", file);
+    }
+    axios.post("/postUpload", formData)
+    .then(async (res) => {
+      console.log(res.data);
+      // delay to avoid searching for initial post folder
+      await setTimeout(() => { window.location.reload(false); }, 1);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
 
   return (
-    <div className='addPostComponent'>
-      <form onSubmit={handleFileSubmit}>
-        <label>
-          Make a Post: <br />
-          Title: <input name="title" type="text" /><br />
-          Body: <input name="body" type="text" /><br />
-          Upload a File: <input name="file" type="file" /><br />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
+    <Modal 
+    {...props}
+    size="lg"
+    aria-labelledby="contained-modal-title-vcenter"
+    centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          Make a Post
+        </Modal.Title>
+      </Modal.Header>
+        <Modal.Body>
+        <Form onSubmit={handlePostSubmit}>
+          <Form.Group className="mb-3" controlId="postTitle">
+            <Form.Label>Title</Form.Label>
+            <Form.Control placeholder="Enter post title" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="postBody">
+            <Form.Label>Body</Form.Label>
+            <Form.Control as="textarea" placeholder="Enter post body" />
+          </Form.Group>
+          <Form.Group controlId="formFile" className="mb-3">
+            <Form.Label>Upload an image (optional)</Form.Label>
+            <Form.Control type="file" size="sm" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+        <div className='addPostComponent'>
+          <form onSubmit={handlePostSubmit}>
+            <label>
+              Make a Post: <br />
+              Title: <input name="title" type="text" /><br />
+              Body: <input name="body" type="text" /><br />
+              Upload a File: <input name="file" type="file" /><br />
+            </label>
+            {/* <input type="submit" value="Submit" /> */}
+          </form>
+        </div>
+        </Modal.Body>
+      <Modal.Footer>
+        <Button form="addPostForm" type="submit">Submit</Button>
+      </Modal.Footer>
+    </Modal>
   )
 }
 
