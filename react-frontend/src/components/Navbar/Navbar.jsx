b7f61e8bab93ec1c6763aef40eeb18839b963519
Pongchaiwat <gun.pongchaiwat@gmail.com>
2022-03-23 22:23:22 -0400

    connect to metamask wallet

--- react-frontend/src/components/Navbar/Navbar.jsx
+++ react-frontend/src/components/Navbar/Navbar.jsx
@@ -5,39 +5,20 @@ import AddPostComponent from '../AddPostComponent/AddPostComponent';
 import { AuthContext } from '../../App';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Navbar.scss'
-import { Web3ReactProvider, useWeb3React } from '@web3-react/core';
-import { Web3Provider } from '@ethersproject/providers';
-import { injected, walletconnect, resetWalletConnector, walletlink } from '../../utils/connectors';
-import { getContract } from '../../utils/contract';
+import { useWeb3React } from '@web3-react/core';
 
-import { ethers } from 'ethers';
+import { injected } from '../../utils/connectors';
+
+
+
 
 function Navbar() {
   const context = useContext(AuthContext);
   const [userWalletAddress] = context;
   const [usernameModalShow, setUsernameModalShow] = useState(false);
   const [addPostModalShow, setAddPostModalShow] = useState(false);
-  //const contractAddr = "0xDAF4a09A7184Ba13E8e28Ea6a2CEfd8995887451";
-  //connector, library, chainId, account, activate, deactivate
 	const web3reactContext = useWeb3React(); 
-  // async function connect() {
-  //   await window.ethereum.enable()
-  //   const provider = new ethers.providers.Web3Provider(window.ethereum);
-  //   const signer = provider.getSigner(0);
-  //   const contract = await new ethers.Contract(contractAddr, abi.abi, signer);
-
-  //   const address = await signer.getAddress();
-  //   const balance = await provider.getBalance(userWalletAddress);
-  //   // const count = await contract.register();
-  //   console.log({address,balance,contract});
-  // }
-
-  // connect();
-  const getLibrary = (provider) => {
-		const library = new Web3Provider(provider, 'any');
-		library.pollingInterval = 15000;
-		return library;
-	};
+  
   //web3react metamask
 	const connectMetamaskSimple = async () => {
     
@@ -71,14 +52,14 @@ function Navbar() {
           show={addPostModalShow}
           onHide={() => setAddPostModalShow(false)}
         />
-        <Web3ReactProvider getLibrary={getLibrary}>
+        
 				<button
 					className=""
 					onClick={connectMetamaskSimple}
 				>
 					Connect Metamask
 				</button>
-        </Web3ReactProvider>
+        
 			  
         
       </div>
